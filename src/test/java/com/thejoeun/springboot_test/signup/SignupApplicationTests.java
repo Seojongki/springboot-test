610c2c3ef5f9fbc751f2bd7ad955911c733f5395
Seojongki <jongkis@gmail.com>
2023-09-01 16:00:54 +0900

    spring test

---
+++ src/test/java/com/thejoeun/springboot_test/signup/SignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejoeun.springboot_test.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
